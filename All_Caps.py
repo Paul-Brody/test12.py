8653e874c4a4e4ec5346340a30d32478f12b38b7
Paul Brody <paul.brody@my.jcu.edu.au>
2016-09-01 12:04:11 +1000

    Got the working of print from dict

--- All_Caps.py
+++ All_Caps.py
@@ -1,9 +1,15 @@
 state_names = {"QLD": "Queensland", "NSW":"New South Wales", "VIC":"Victoria", "SA":"South Australia", "WA":"Western Australia", "NT":"Northern Teritory"}
+#state_names = {'QLD': 101, 'NSW': 102, 'WA': 103}
 
-for short_state in state_names:
-   # print("{}  is  {}".format[state, state_names[state]])
-    print(state_names)
 
+#for short_state in state_names:
+
+    # print("{}  is  {}".format[state, state_names[state]])
+    # print(state_names)
+# table = {'Sjoerd': 4127, 'Jack': 4098, 'Dcab': 7678}
+
+for name, state in state_names.items():
+    print('{0:8} ==>      {1:}'.format(name, state))
 
 
 
@@ -11,12 +17,7 @@ for short_state in state_names:
 # ull_state_name = input("Full state Name: ")
 
 # vstate_names
-''' for key in state_names:
-    state_names[word] = state_names.get(word, 0) + 1
-'''
 
-'''
-NSW is New South Wales
-QLD is Queensland
-TAS is Tasmania
-'''
+# for name, state in state_names.items():
+#    print("{}  is  {1:12}".format(name, state) )
+# {0:2d} {1:3d} {2:4d}
